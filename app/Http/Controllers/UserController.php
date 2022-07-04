79084527fb962978e6230315d4214ce874789af3
darwish <ahmed.darwish@awfarmarket.com>
2022-07-05 00:25:46 +0200

    add resrvation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -43,7 +44,7 @@ class UserController extends Controller
     public function register(Request $request)
     {
 
-        $validated = $request->validate([
+        $validated = Validator::make($request->all(),[
             "name" => "required",
             "email" => "required|unique:users",
             "phone" => "required|unique:users",
